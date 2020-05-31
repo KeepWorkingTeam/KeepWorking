aac87514730aea7496e3e6ed7bbc2381de155ea9
Hatulmadan <katiq@inbox.ru>
2020-05-31 19:45:54 +0300

    DB developing

---
+++ DBManager/DBManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DBManager
+{
+    public class DBManager
+    {
+        string path = @"..\..\..\DBManager\DB.xml";
+        public void WriteToDB(string fileName, TimeSpan duration)
+        {
+            XDocument DBXmlVersion = XDocument.Load(path);
+
+            FileHistory fileHistory = GetFileHistory(fileName);
+
+            if (fileHistory == null)
+            {
+                DBXmlVersion.Element("timespans").Add(new XElement("timespan",
+                        new XElement("name", fileName),
+                        new XElement("today", duration.ToString()),
+                        new XElement("thisweek", duration.ToString()),
+                        new XElement("totaltime", duration.ToString())));
+            }
+            else
+            {
+                foreach (XElement file in DBXmlVersion.Element("timespans").Elements("timespan"))
+                {
+                    string name = file.Element("name").Value;
+
+                    if (name == fileName)
+                    {
+                        TimeSpan newToday = duration + TimeSpan.Parse(file.Element("today").Value);
+                        TimeSpan newThisWeek = duration + TimeSpan.Parse(file.Element("thisweek").Value);
+                        TimeSpan newTotalTime = duration + TimeSpan.Parse(file.Element("totaltime").Value);
+
+                        file.Element("today").Value = newToday.ToString();
+                        file.Element("thisweek").Value = newThisWeek.ToString();
+                        file.Element("totaltime").Value = newTotalTime.ToString();
+
+                        break;
+                    }
+                }
+            }
+            DBXmlVersion.Save(path);
+        }
+
+        public FileHistory GetFileHistory(string fileName)
+        {
+            XDocument DBXmlVersion = XDocument.Load(path);
+
+            var items = from element in DBXmlVersion.Element("timespans").Elements("timespan")
+                        where element.Element("name").Value == fileName
+                        select new FileHistory
+                        {
+                            FileName = element.Element("name").Value,
+                            TodayTime = TimeSpan.Parse(element.Element("today").Value),
+                            ThisWeekTime = TimeSpan.Parse(element.Element("thisweek").Value),
+                            TotalTime = TimeSpan.Parse(element.Element("totaltime").Value)
+                        };
+
+            return (FileHistory)items;
+        }
+
+        public void StartNewDay()
+        {
+            XDocument DBXmlVersion = XDocument.Load(path);
+
+            foreach (XElement file in DBXmlVersion.Element("timespans").Elements("timespan"))
+            {
+                file.Element("today").Value = TimeSpan.FromSeconds(0).ToString();
+            }
+        }
+
+        public void StartNewWeek()
+        {
+            XDocument DBXmlVersion = XDocument.Load(path);
+
+            foreach (XElement file in DBXmlVersion.Element("timespans").Elements("timespan"))
+            {
+                file.Element("thisweek").Value = TimeSpan.FromSeconds(0).ToString();
+            }
+        }
+    }
+}
---
+++ DBManager/FileHistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBManager
+{
+    public class FileHistory
+    {
+        public string FileName { get; set; } 
+        public TimeSpan TodayTime { get; set; }
+        public TimeSpan ThisWeekTime { get; set; }
+        public TimeSpan TotalTime { get; set; }
+    }
+}
